80bd9f0fae150ca5d08b92b7b97147d010bf8cf0
Lew <andrade.lew@gmail.com>
2021-03-02 17:23:51 -0300

    Created ChessPosition

---
+++ ConsoleChess/Chess/ChessPosition.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleChess.Table;
+
+namespace ConsoleChess.Chess
+{
+    public class ChessPosition
+    {
+        public int Row { get; set; }
+        public char Column { get; set; }
+
+        public ChessPosition(int row, char column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Position ToPosition() => new Position(8 - Row, Column - 'a');
+
+        public override string ToString() => $"{Column}{Row}";
+    }
+}
