44e1fc914c7eb2351f7ee7dee4458a7f13146979
Lew <andrade.lew@gmail.com>
2021-03-02 01:54:59 -0300

    Added Position Class

---
+++ ConsoleChess/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleChess.Board;
+
+namespace ConsoleChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position P;
+            P = new Position(3, 4);
+            Console.WriteLine(P);
+        }
+    }
+}
